8872a805d072eb3d821d8fe820778ac2fc199f05
RaymondTomcat <swordart2050@gmail.com>
2023-04-01 14:51:31 +0430

    Calling and separating pages and files

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -1,19 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>وبلاگ {{$title ?? ''}}</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+    @include('components.include.meta')
     <!-- Styles -->
-
+    @include('components.include.style')
 </head>
 <body class="antialiased">
     {{$slot}}
+    @include('components.include.javascript')
 </body>
 </html>
